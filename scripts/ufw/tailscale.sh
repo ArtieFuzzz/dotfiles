0dc14070b665c3d1731cc40e91b416a70b7e7baf
Artie Fuzzz <48877462+ArtieFuzzz@users.noreply.github.com>
2022-10-24 13:00:23 +1100

    Update UFW Tailscale script

--- scripts/ufw/tailscale.sh
+++ scripts/ufw/tailscale.sh
@@ -1,1 +1,20 @@
+#!/bin/bash
+
+if [[ "$EUID" != 0 ]]
+then
+    echo "You must run this command as sudo..."
+    exit
+fi
+
+if ! command -v ufw &> /dev/null
+then
+    echo "ufw is not installed"
+    echo "use 'sudo apt-get install ufw' to install ufw"
+    exit
+fi
+
+ufw allow in on tailscale0
+ufw allow 41641/udp
+
+echo "If ufw is not already enabled...\nufw enable"
 
