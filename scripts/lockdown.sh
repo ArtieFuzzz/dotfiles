9fe1c851f927d6f598d68e7b64fbe9db698cf099
Artie Fuzzz <48877462+ArtieFuzzz@users.noreply.github.com>
2022-10-24 12:59:51 +1100

    Add lockdown script

---
+++ scripts/lockdown.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+# This script locks down the server so only Tailscale connections are allowed
+# USE THIS SCRIPT WITH CAUTION AS IT MAY LOCK YOU OUT OF YOUR SERVER
+
+if [[ "$EUID" != 0 ]]
+then
+    echo "You must run this command as sudo..."
+    exit
+fi
+
+if ! command -v tailscale &> /dev/null
+then
+    echo "Tailscale is not installed on this system..."
+    echo "Setup Tailscale: https://tailscale.com/download"
+    exit
+fi
+
+curl -s https://github.com/ArtieFuzzz/dotfiles/blob/main/scripts/ufw/tailscale.sh | bash
+
+ufw default deny incoming
+ufw default allow outgoing
+
+echo "Locked down server!"
+echo "To unlock:\n"
+echo "ufw default allow incoming"
